d2f1e2864956347cb2b5ff46ca2e26edad66d3c8
Felix Meziere <felix.meziere@gmail.com>
2019-03-31 11:08:34 +0100

    fastlane-env with codepush

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import codePush from 'react-native-code-push';
 
-AppRegistry.registerComponent(appName, () => App);
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_SUSPEND,
+  minimumBackgroundDuration: 60,
+};
+const CodePushedApp = codePush(codePushOptions)(App);
+
+AppRegistry.registerComponent(appName, () => CodePushedApp);
---
+++ ios/Pods/Target Support Files/Pods-frigoDemo-tvOSTests/Pods-frigoDemo-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_frigoDemo_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_frigoDemo_tvOSTests
+@end
