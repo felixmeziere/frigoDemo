301555deea2fbe90b6fc2311c8db88e1ef0e3f0b
Felix Meziere <felix.meziere@gmail.com>
2019-03-31 16:54:19 +0100

    config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          App: './src/App',
+          components: './src/components',
+          environment: './src/environment',
+          'redux/app': './src/redux/app',
+        },
+      },
+    ],
+  ],
 };
